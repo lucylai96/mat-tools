c729076d54ca0f6ca27f8301f0ab3cc606186356
Lucy Lai <lucylai.lxl@gmail.com>
2020-07-19 12:20:13 -0400

    new functions

--- circle.m
+++ circle.m
@@ -3,6 +3,6 @@ hold on
 th = 0:pi/50:2*pi;
 xunit = r * cos(th)+ x;
 yunit = r * sin(th) + y;
-h = plot(xunit(1:25), yunit(1:25));
+h = plot(xunit(1:25), yunit(1:25),'LineWidth',2);
 
 end
--- shuffle.m
+++ shuffle.m
@@ -1,108 +1,61 @@
-function X = Shuffle(Arg1, Arg2, Arg3)  %#ok<INUSD,STOUT>
-% Random permutation of array elements
-% SHUFFLE works in Inplace, Index and Derange mode:
+function [ Y I ] = shuffle( X , DIM )
+%SHUFFLE    Shuffles elements.
+%   For vectors, SHUFFLE(X) shuffles the elements of X.
+%   For matrices, SHUFFLE(X) shuffles the rows of X.
+%   For N-D arrays, SHUFFLE(X) shuffles along the first non-singleton
+%   dimension of X.
 %
-% 1. INPLACE MODE: Y = Shuffle(X, Dim)   - - - - - - - - - - - - - - - - - - - -
-% The elements of the input array are randomly re-ordered along the specified
-% dimension. In opposite to X(RANDPERM(LENGTH(X)) no temporary memory is used,
-% such that SHUFFLE is much more efficient for large arrays.
-% INPUT:
-%   X: Array of any size. Types: DOUBLE, SINGLE, CHAR, LOGICAL,
-%      (U)INT64/32/16/8. The processed dimension must be shorter than 2^32
-%      elements. To shuffle larger or complex arrays, use the Index mode.
-%   Dim: Dimension to operate on.
-%      Optional, default: [], operate on first non-singleton dimension.
-% OUTPUT:
-%   Y: Array of same size and type as X, but with shuffled elements.
+%   SHUFFLE(X,DIM) shuffles along the dimension DIM. If DIM is 0,
+%   then all elements of X are shuffled linearly (independently of
+%   any dimension).
+% 
+%   [Y,I] = SHUFFLE(X,DIM) also returns an index vector I.
+%   If X is a vector, then Y = X(I).
+%   If X is a matrix and DIM=1, then Y = X(I,:).
 %
-% 2. INDEX MODE: Index = Shuffle(N, 'index', NOut)   - - - - - - - - - - - - - -
-% A vector of shuffled indices is created as by RANDPERM, but faster and using
-% the smallest possible integer type to save memory. The number of output
-% elements can be limited. This method works for cells, structs or complex
-% arrays.
-% INPUT:
-%   N:      Numeric scalar >= 0.
-%   String: 'index'.
-%   NOut:   Optional, number of outputs, NOut <= N. Default: N.
-% OUTPUT:
-%   Index:  [1 x nOut] vector containing the shuffled elements of the vector
-%           [1:N] vector. To limit the memory usage the smallest possible type
-%           is used: UINT8/16/32 or INT64.
+%   Examples:
+%         X = [1 2 3; 4 5 6; 7 8 9]
+%         X =
+%              1     2     3
+%              4     5     6
+%              7     8     9
 %
-% 3. DERANGEMENT MODE: Index = Shuffle(N, 'derange', NOut)   - - - - - - - - - -
-% Equivalent to Index mode, but all elements Index[i] ~= i.
+%         [Y,I] = shuffle(X)
+%         Y =
+%              7     8     9
+%              4     5     6
+%              1     2     3
+%         I =
+%              3     2     1
+% 
+%         [Y,I] = shuffle(X,2)
+%         Y =
+%              1     3     2
+%              4     6     5
+%              7     9     8
+%         I =
+%              1     3     2
+%   Created: Ankur Jain (encorejane@gmail.com) - 01/30/2010
 %
-% CONTROL COMMANDS:  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-% Shuffle(S, 'seed'): S is either empty, scalar or a [1 x 4] DOUBLE vector to
-%      set the random number  generator to a certain state. 4 integers between 0
-%      and 2^32-1 are recommended for maximum entropy. If S is empty the default
-%      seed is used.
-% Shuffle([], 'lock'): Lock Mex in the memory such that "clear all" does not
-%      reset the random  number generator. If the Mex was compiled with
-%      -DAUTOLOCK it is locked automatically.
-% Shuffle([], 'unlock'): Unlock the Mex file to allow a recompilation.
-% Shuffle(): Display compiler settings and status: Fast/exact integer, accept
-%      cell/struct, current lock status, auto-lock mode.
-%
-% EXAMPLES:
-%   Shuffle(12345678901, 'seed');
-%   R = Shuffle(1:8)             %  [5, 8, 7, 4, 2, 6, 3, 1]
-%   R = Shuffle('abcdefg')       %  'gdfbcea'
-%   R = Shuffle([1:4; 5:8], 2)   %  [3, 4, 2, 1;  8, 6, 7, 5]
-%   I = Shuffle(8, 'index');     %  UINT8([3, 6, 5, 2, 4, 7, 8, 1])
-% Choose 10 different rows from a 1000 x 100 matrix:
-%   X = rand(1000, 100);       Y = X(Shuffle(1000, 'index', 10), :);
-% Operate on cells or complex arrays:
-%   C = {9, 's', 1:5};         SC = C(Shuffle(numel(C), 'index'));
-%   M = rand(3) + i * rand(3); SM = M(:, Shuffle(size(C, 2), 'index'))
-%
-% NOTES: Shuffle(X) is about 50% to 85% faster than: Y = X(randperm(numel(X)).
-%   It uses the Knuth-shuffle algorithm, also known as Fisher-Yates-shuffle.
-%   The random numbers are created by the cute KISS algorithm of George
-%   Marsaglia, which has a period of about 2^124 (~10^37).
-%   More notes in Shuffle.c.
-%
-% COMPILATION: See Shuffle.c
-%
-% Tested: Matlab 6.5, 7.7, 7.8, WinXP, 32bit
-%         Compiler: LCC2.4/3.8, BCC5.5, OWC1.8, MSVC2008
-% Assumed Compatibility: higher Matlab versions, Mac, Linux, 64bit
-% Author: Jan Simon, Heidelberg, (C) 2010-2011 j@n-simon.de
-%
-% See also RAND, RANDPERM.
-% FEX:
 
-% $JRev: R-m V:012 Sum:Q65/zXZqnvFX Date:07-Mar-2011 00:49:06 $
-% $License: BSD (use/copy/change/redistribute on own risk, mention the author) $
-% $UnitTest: uTest_Shuffle $
-% $File: Tools\GLSets\Shuffle.m $
+    error(nargchk(1,2,nargin));
 
-% History see Shuffle.c
+    szi = size(X);
+    if nargin < 2, DIM = find(szi,1); end
 
-% Some Matlab algorithms for the Knuth-Shuffle: --------------------------------
-% They are faster than Matlab's RANDPERM methods, but the MEX is recommended!
+    if DIM > numel(szi)
+        Y = X;
+        I = 1;
+        return
+    end
 
-% n = numel(X);
-% for i = 2:n      % Knuth shuffle in forward direction:
-%    w    = ceil(rand * i);   % 1 <= w <= i
-%    t    = X(w);
-%    X(w) = X(i);
-%    X(i) = t;
-% end
- 
-% for i = n:-1:2   % Knuth shuffle in backward direction:
-%    w    = ceil(rand * i);   % 1 <= w <= i
-%    t    = X(w);
-%    X(w) = X(i);
-%    X(i) = t;
-% end
+    if DIM == 0
+        Y = reshape(X(randperm(numel(X))), szi);
+        return;
+    end
 
-% for i = 1:nOut   % Limit output:
-%    w    = ceil(rand * (n - i + 1)) + (i - 1);   % i <= w <= n
-%    t    = X(w);
-%    X(w) = X(i);
-%    X(i) = t;
-% end
-% X = X(1:nOut);
-
-error(['JSimon:', mfilename, ':NoMex'], 'Need compiled mex file!');
+    inds(1:numel(szi)) = {':'};
+    I = randperm(szi(DIM));
+    inds{DIM} = I;
+    Y = X(inds{:});
+end
